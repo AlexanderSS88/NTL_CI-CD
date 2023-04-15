d35dbd4491fbd5946647f265b7a0b913e20f3ad4
AlexanderSS88 <t214oh@mail.ru>
2023-04-15 22:51:18 +0300

    fix linting

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase, Client
 
+
 class TestMyApp(TestCase):
+
     def test_my_stocks(self):
         client = Client()
         response = client.get('/stocks/')
         self.assertEqual(response.status_code, 200)
+
     def test_my_products(self):
         client = Client()
         response = client.get('/products/')
         self.assertEqual(response.status_code, 200)
-
