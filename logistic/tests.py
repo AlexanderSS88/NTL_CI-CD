23a67672941e1ac6cd710ab376e0ed90b2a363af
AlexanderSS88 <t214oh@mail.ru>
2023-04-16 22:39:39 +0300

    fix9

--- logistic/tests.py
+++ logistic/tests.py
@@ -5,10 +5,10 @@ class TestMyApp(TestCase):
 
     def test_my_stocks(self):
         client = Client()
-        response = client.get('/stocks/')
+        response = client.get('http://127.0.0.1:8000/api/v1/stocks/')
         self.assertEqual(response.status_code, 200)
 
     def test_my_products(self):
         client = Client()
-        response = client.get('/products/')
+        response = client.get('http://127.0.0.1:8000/api/v1/products/')
         self.assertEqual(response.status_code, 200)
